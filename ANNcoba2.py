f595740d9f818581a139ba7193396ffbcfadec0e
aldofadhila027 <silver_elite@rocketmail.com>
2016-07-28 16:42:35 +0700

    Update ANNcoba2.py

--- ANNcoba2.py
+++ ANNcoba2.py
@@ -7,7 +7,86 @@ import timeit
 import numpy as np 
 #import Theano 
 import theano.tensor as T
+class LogisticRegression(object):
+    def __init__ (self, input, n_in,n_out) :
+        self.W = theano.shared(
+            value=numpy.zeros(
+                (n_in, n_out),
+                dtype=theano.config.floatX
+            ),
+            name='W',
+            borrow=True
+        )
+        # initialize the biases b as a vector of n_out 0s
+        self.b = theano.shared(
+            value=numpy.zeros(
+                (n_out,),
+                dtype=theano.config.floatX
+            ),
+            name='b',
+            borrow=True
+        )
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+        
+        self.params = [self.W , self.b]
+        self.input  = input 
+    def negative_log_likelihood(self, y):
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+    def errors(self, y):
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError(
+                'y should have the same shape as self.y_pred',
+                ('y', y.type, 'y_pred', self.y_pred.type)
+            )
+        # check if y is of the correct datatype
+        if y.dtype.startswith('int'):
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
+    def load_data(dataset) :
+        data_dir, data_file = os.path.split(dataset)
+        if data_dir == "" and not os.path.isfile(dataset):
+            # Check if dataset is in the data directory.
+            new_path = os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "data",
+                dataset
+             )
+            if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
+                dataset = new_path
+        if (not os.path.isfile(dataset)) and data_file == 'mnist.pk1.gz':
+            from six.moves import urllib
+            origin = (
+                'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+            )
+            print('Downloading data from %s' % origin)
+            urllib.request.urlretrieve(origin, dataset)
+        
+    print('... loading data')
+    with gzip.open(dataset, 'rb') as f:
+        try:
+            train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+        except:
+            train_set, valid_set, test_set = pickle.load(f)
+    def shared_dataset(data_xy, borrow = True):
+        data_x, data_y = data_xy
+        shared_x = theano.shared(numpy.asarray(data_x,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(numpy.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+         return shared_x, T.cast(shared_y, 'int32')
+    test_set_x, test_set_y = shared_dataset(test_set)
+    valid_set_x, valid_set_y = shared_dataset(valid_set)
+    train_set_x, train_set_y = shared_dataset(train_set)
 
+    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
+            (test_set_x, test_set_y)]
+    return rval
 class HiddenLayer(object):
     def _init__(self, rng, input, n_in,n_out, W = None, b=None,activation = T.tanh):
 
